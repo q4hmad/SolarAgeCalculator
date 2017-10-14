7831af421a694fd0b6cd8d612c94f82398d8219d
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-10-14 13:42:01 -0700

    interface file for date of birth

--- js/solarAgeCalculator-interface.js
+++ js/solarAgeCalculator-interface.js
@@ -2,27 +2,11 @@ import { Solar } from './../js/solarAgeCalculator.js';
 
 $(document).ready(function() {
   $("form#enterAge").submit(function(event) {
+    console.log("helo");
     event.preventDefault();
     var age = parseFloat($("#age").val());
-
+    console.log(age);
     var ageChecker = new Solar(age);
-
     $("#theirAge").text(ageChecker.checkSeconds());
   });
-
-$(document).ready(function(){
-  $("#check").on('click', function() {
-    var moment = moment();
-    var now = moment($("#dateOfBirth").val());
-    var diff = now.diff(moment);
-    $('#time').text("diff between now and moment in msecs-", diff);
-
-
-  });
 });
-
-
-
-
-// let [day,month,year] = $('#datePicker').val().split('-');
-//
